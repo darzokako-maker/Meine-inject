85f96be041515fa6b8e9437ccb66af3631320962
darzokako-maker <darzokako@gmail.com>
2026-04-12 16:31:10 +0300

    Update message box title in DLL injection notification

--- cheat.cpp
+++ cheat.cpp
@@ -1,12 +1,14 @@
 #include <windows.h>
 
+// Derleyiciye User32 kütüphanesini kullanmasını söyler (Hata çözümü)
+#pragma comment(lib, "user32.lib")
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-        // DLL enjekte edildigi an bu mesaj kutusu cikar
-        MessageBoxA(NULL, "DLL Basariyla Enjekte Edildi!", "Meine Injector", MB_OK | MB_ICONINFORMATION);
+        // Enjekte olduğu an mesaj kutusu çıkarır
+        MessageBoxA(NULL, "DLL Basariyla Enjekte Edildi!", "Meine-Inject", MB_OK | MB_ICONINFORMATION);
         break;
     }
     return TRUE;
 }
-
